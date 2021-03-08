7be62f6f518b2281e5bfa9b7677bb395eb269b5c
Jeff <chx.787dreamliner@gmail.com>
2021-03-08 12:43:28 -0800

    Fixed local logging error

--- DHT22ReadOnly_Cloud.py
+++ DHT22ReadOnly_Cloud.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 
-#thingspeak imports 
-from urllib.request import urlopen 
+#thingspeak imports
+from urllib.request import urlopen
 
 #importing threading
 import _thread
-#import Time 
+#import Time
 import time
 from datetime import datetime
 
 #importing for the AdaFruit Board
 import board
 import adafruit_dht
+
+import os
+import csv
 
 #----------------------------------------------------------
 #-----------Sensor Global Variables------------------------
@@ -21,17 +24,17 @@ dhtDevice0 = adafruit_dht.DHT22(board.D4)
 dhtDevice1 = adafruit_dht.DHT22(board.D18)
 
 
-#global variables for the sensor on D4 pin 
+#global variables for the sensor on D4 pin
 sensor_fault0 = bool(0)
-temp0 = 0 
-humid0 = 0 
+temp0 = 0
+humid0 = 0
 
-#global variables for the sensors 
+#global variables for the sensors
 sensor_fault1 = bool(0)
 temp1 = 0
-humid1 = 0 
+humid1 = 0
 
-#global for the dislplay and logging 
+#global for the dislplay and logging
 temp_last_avg=0
 temp_avg = 0
 humid_last_avg = 0
@@ -42,17 +45,17 @@ filetimer = datetime.now()
 
 #-----------------------------------------------------------------
 #----------------ThingSpeak Global Variables----------------------
-#ThingSpeak credentrials 
-myAPI = '4M4MSZ8ZYP18AU3E' 
+#ThingSpeak credentrials
+myAPI = '4M4MSZ8ZYP18AU3E'
 # URL where we will send the data, Don't change it
-baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI 
+baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI
 
 
 #______________________________________________________________________________________________________________________________
-#------------------Everything Between lines for Sensor read Data and error checking--------------------------------------------- 
+#------------------Everything Between lines for Sensor read Data and error checking---------------------------------------------
 
 
-# DHT22 Sensor on Pin D4 
+# DHT22 Sensor on Pin D4
 def sensor0( threadName, delay):
     global temp0
     global humid0
@@ -67,18 +70,18 @@ def sensor0( threadName, delay):
                 humidity = dhtDevice0.humidity
                 if (abs(temp_avg - temperature_c ) < 4):
                     fault = bool(0)
-                break            
+                break
             except RuntimeError as error:
                 # Errors happen fairly often, DHT's are hard to read, just keep going
                 time.sleep(delay)
-                
+
             except Exception as error:
                 dhtDevice0.exit()
                 raise error
             if (x > 3):
                 fault = bool(1)
                 temperature_c = 0
-                humidity = 0 
+                humidity = 0
                 break
 
         temp0 = temperature_c
@@ -87,42 +90,42 @@ def sensor0( threadName, delay):
         time.sleep(delay)
 
 
-# DHT22 Sensor on Pin D18  
+# DHT22 Sensor on Pin D18
 def sensor1( threadName, delay):
     global temp1
     global humid1
     global sensor_fault1
     global temp_avg
     fault = bool(0)
-    while True: #runs forever 
-        for x in range(5):#tries 5 times before thorwing fault for sensor 
+    while True: #runs forever
+        for x in range(5):#tries 5 times before thorwing fault for sensor
             try:
                 # Print the values to the serial port
                 temperature_c = dhtDevice1.temperature
                 humidity = dhtDevice1.humidity
-                if (abs(temp_avg - temperature_c ) < 4): # if the sensor is within a certain range it will be used again 
+                if (abs(temp_avg - temperature_c ) < 4): # if the sensor is within a certain range it will be used again
                     fault = bool(0)
-                break            
+                break
             except RuntimeError as error:
                 # Errors happen fairly often, DHT's are hard to read, just keep going
                 time.sleep(delay)
-                
+
             except Exception as error:
                 dhtDevice1.exit()
                 raise error
             if (x > 3):
                 fault = bool(1)
                 temperature_c = 0
-                humidity = 0 
+                humidity = 0
                 break
-        #stores to global variables once it has gotten a proper value otherwise if faulty sets to 0, 0 
+        #stores to global variables once it has gotten a proper value otherwise if faulty sets to 0, 0
         temp1 = temperature_c
         humid1 = humidity
         sensor_fault1 = fault
-        time.sleep(delay)        
+        time.sleep(delay)
 
 
-# to calculate the avg and error check between two sensors 
+# to calculate the avg and error check between two sensors
 def avg(threadName, delay):
     global temp_avg #uses all global variables which are also used for uploading and display
     global humid_avg
@@ -134,18 +137,18 @@ def avg(threadName, delay):
     global humid1
     global sensor_fault0
     global sensor_fault1
-    
-    while True: #runs the avg loop forever 
-        
-        temp_last_avg = temp_avg #stores current avgerage as last avg 
+
+    while True: #runs the avg loop forever
+
+        temp_last_avg = temp_avg #stores current avgerage as last avg
         humid_last_avg = humid_avg
 
-        # mutltiple statments checking different sinarios for faulty sensor 
+        # mutltiple statments checking different sinarios for faulty sensor
         if (sensor_fault0 == False and sensor_fault1 == False) :#both good
             tempdiff = abs(temp0-temp1)
             humiddiff = abs(humid0-humid1)
 
-            if (tempdiff < 4 and humiddiff < 10) :# they are roughly the same value 
+            if (tempdiff < 4 and humiddiff < 10) :# they are roughly the same value
                 temp_avg = (temp0 + temp1) / 2
                 humid_avg = (humid0 + humid1) / 2
 
@@ -155,24 +158,24 @@ def avg(threadName, delay):
 
         elif (sensor_fault0 == True and sensor_fault1 == False): #D4 Bad D18 OK
             temp_avg = (temp1 + temp_last_avg) / 2
-            humid_avg = (humid1 + humid_last_avg) / 2 
-        else: #both Bad set to 0 , 0 
+            humid_avg = (humid1 + humid_last_avg) / 2
+        else: #both Bad set to 0 , 0
             temp_avg = 0
             humid_avg = 0
 
-        
-        # prints to terminal for error checking 
+
+        # prints to terminal for error checking
         print(
                 "Temp_avg:  {:.1f} C    Humidity_avg: {:.1f}%   sensor0: {}   Sensor1: {} ".format(
                     temp_avg, humid_avg,sensor_fault0,sensor_fault1
                 )
-            ) 
-        time.sleep(delay)#sleeps for set delay time 
+            )
+        time.sleep(delay)#sleeps for set delay time
 
 
 
 
-# 
+#
 #-----------------------------------End of sensor Data read and error Check------------------------------------------------------------------------
 #______________________________________________________________________________________________________________
 #_________________________________________________________________________________________________________________
@@ -203,15 +206,16 @@ def local(threadName, delay):
     global temp1
     global humid1
     global filetimer
+    now = datetime.now()
     while True:
         try:
             file = open("/home/pi/data_log.csv", "a")
             if os.stat("/home/pi/data_log.csv").st_size == 0:
-                file.write("Time,S1TempC,S1Humid,S2TempC,S2umid,\n")
-            
-            file.write(str(now)+","+str(temp0)+","+str(humd0)+str(temp1)+","+str(humd1)+"\n")
+                file.write("Time,S1TempC,S1Humid,S2TempC,S2Humid,\n")
+
+            file.write(str(now)+","+str(temp0)+","+str(humid0)+str(temp1)+","+str(humid1)+"\n")
             file.flush()
-            
+
             if now.hour > filetimer.hour:
                 file.close()
                 # check if directory exists
@@ -219,17 +223,17 @@ def local(threadName, delay):
                     filetimer.year, filetimer.month, filetimer.day)
                 ):
                     # make directory if not exist
-                    os.makedirs("/home/pi/{}/{}/{}/".format( 
+                    os.makedirs("/home/pi/{}/{}/{}/".format(
                         filetimer.year, filetimer.month, filetimer.day)
                     )
                 # move file to according folder
-                os.rename("/home/pi/data_log.csv","/home/pi/{}/{}/{}/{}hr.csv".format(
+                os.rename("/home/pi/data_log.csv","~/{}/{}/{}/{}hr.csv".format(
                         filetimer.year, filetimer.month, filetimer.day, filetimer.hour
                     )
                 )
                 filetimer = datetime.now()
         except:
-            print("Connection Failed")
+            print("Logging Failed")
         time.sleep(delay)
 
 #---------------------------------End Of GUI -------------------------------------------------------------------------------
@@ -259,12 +263,12 @@ except:
 
 
 
-#_______________________main loop for the programe____________________________________________ 
-while 1:#True loop to run code forever 
-    
-    #keeps threads running in background           
-    pass 
+#_______________________main loop for the programe____________________________________________
+while 1:#True loop to run code forever
+
+    #keeps threads running in background
+    pass
 #-------------------------End of Main Loop-----------------------------------------------------
 #_______________________________________________________________________________________________
 
-#end of code 
+#end of code
